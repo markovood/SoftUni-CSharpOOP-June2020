af0387a81440ab9f75c919ca06343e8e7b4bd1a5
Georgi Markov <markov_ood@abv.bg>
2020-07-04 19:26:41 +0300

    Completed Encapsulation Lab...

---
+++ Encapsulation - Lab/Team/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Team team = new Team("SoftUni");
+
+            int numbOfpersons = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numbOfpersons; i++)
+            {
+                string[] cmdArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                Person player = new Person(cmdArgs[0],
+                                            cmdArgs[1],
+                                            int.Parse(cmdArgs[2]),
+                                            decimal.Parse(cmdArgs[3]));
+                team.AddPlayer(player);
+            }
+
+            Console.WriteLine(team);
+        }
+    }
+}

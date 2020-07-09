d4b207f27b53a2719302903f30113c88ef9f7a4b
Georgi Markov <markov_ood@abv.bg>
2020-07-09 23:07:46 +0300

    Completed Polymorphism - Lab...

---
+++ Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.Radius = radius;
+        }
+
+        public double Radius 
+        {
+            get => this.radius;
+            private set => this.radius = value;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * (this.Radius * this.Radius);
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.Radius;
+        }
+
+        public override string Draw()
+        {
+            return "I am a circle.";
+        }
+    }
+}
---
+++ Polymorphism - Lab/Shapes/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var rect = new Rectangle(5d, 6d);
+            var circle = new Circle(3d);
+
+            List<Shape> shapes = new List<Shape>() { rect, circle };
+
+            shapes.ForEach(sh =>
+            {
+                Console.WriteLine(sh.CalculateArea());
+                Console.WriteLine(sh.CalculatePerimeter());
+                Console.WriteLine(sh.Draw());
+            });
+        }
+    }
+}
